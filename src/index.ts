f1949831f1f50f0786ac96cde34ae9019d9ad12e
Sasha Mat <unglud@gmail.com>
2023-08-11 11:57:55 +0200

    feat: retrieve data in chain

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,120 @@ import { openAIApiKey } from "./env.ts";
 import getStore from "./store.ts";
 import loadGitHub from "./loaders/github.ts";
 
-//import { OpenAI } from "https://esm.sh/v130/langchain@0.0.125/llms/openai";
+import { OpenAI } from "https://esm.sh/v130/langchain@0.0.125/llms/openai";
 import { OpenAIEmbeddings } from "https://esm.sh/v130/langchain@0.0.125/embeddings/openai";
-// import { RetrievalQAChain } from "https://esm.sh/v130/langchain@0.0.125/chains";
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  PromptTemplate,
+  SystemMessagePromptTemplate,
+} from "https://esm.sh/v130/langchain@0.0.125/prompts";
+import {
+  RunnablePassthrough,
+  RunnableSequence,
+} from "https://esm.sh/v130/langchain@0.0.125/schema/runnable";
+import { StringOutputParser } from "https://esm.sh/v130/langchain@0.0.125/schema/output_parser";
+import { Document } from "https://esm.sh/v130/langchain@0.0.125/document";
+import { BaseCallbackHandler } from "https://esm.sh/v130/langchain@0.0.125/callbacks";
+import { Serialized } from "https://esm.sh/v130/langchain@0.0.125/load/serializable";
+import {
+  AgentAction,
+  AgentFinish,
+  ChainValues,
+} from "https://esm.sh/v130/langchain@0.0.125/schema";
 
 const docs = await loadGitHub();
 //console.log("docs", docs);
 
-//const model = new OpenAI({ openAIApiKey });
+const model = new OpenAI({ openAIApiKey });
 const embeddings = new OpenAIEmbeddings({ openAIApiKey });
 
 // Load the docs into the vector getStore.ts
-const _vectorStore = await getStore(embeddings, docs);
-/*
-// Initialize a retriever wrapper around the vector store
-const vectorStoreRetriever = vectorStore.asRetriever();
+const vectorStore = await getStore(embeddings, docs);
+const retriever = vectorStore.asRetriever({ verbose: false });
 
-// Create a chain that uses the OpenAI LLM and vector store.
-const chain = RetrievalQAChain.fromLLM(model, vectorStoreRetriever);
-const res = await chain.call({
-  query: "embeddings",
-});
+const systemMessagePrompt = SystemMessagePromptTemplate.fromTemplate(
+  "Answer the question based only on the following context:\n{context}",
+);
+const humanMessagePrompt = HumanMessagePromptTemplate.fromTemplate(
+  "Question: {question}",
+);
 
-console.log(`res`, res);
-*/
+const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+  systemMessagePrompt,
+  humanMessagePrompt,
+]);
+
+type ConversationalRetrievalQAChainInput = {
+  question: string;
+  //chat_history: [string, string][];
+};
+
+const standaloneQuestionChain = RunnableSequence.from([
+  {
+    question: (input: ConversationalRetrievalQAChainInput) => input.question,
+    //chat_history: (input: ConversationalRetrievalQAChainInput) => formatChatHistory(input.chat_history),
+  },
+  PromptTemplate.fromTemplate(
+    /*Chat History:
+{chat_history}*/
+    `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+    
+Follow Up Input: {question}
+Standalone question:`,
+  ),
+  model,
+  new StringOutputParser(),
+]);
+
+const combineDocumentsFn = (docs: Document[], separator = "\n\n") => {
+  const serializedDocs = docs.map((doc) => doc.pageContent);
+  return serializedDocs.join(separator);
+};
+
+const answerChain = RunnableSequence.from([
+  {
+    context: retriever.pipe(combineDocumentsFn),
+    question: new RunnablePassthrough(),
+  },
+  chatPrompt,
+  model,
+]);
+
+const conversationalRetrievalQAChain = standaloneQuestionChain.pipe(
+  answerChain,
+);
+
+export class MyCallbackHandler extends BaseCallbackHandler {
+  name = "MyCallbackHandler";
+
+  handleChainStart(chain: Serialized) {
+    console.log(`Entering new ${chain.id} chain...`, chain);
+  }
+
+  handleChainEnd(output: ChainValues) {
+    console.log("Finished chain.", output);
+  }
+
+  handleAgentAction(action: AgentAction) {
+    console.log(action.log);
+  }
+
+  handleToolEnd(output: string) {
+    console.log(output);
+  }
+
+  handleText(text: string) {
+    console.log(text);
+  }
+
+  handleAgentEnd(action: AgentFinish) {
+    console.log(action.log);
+  }
+}
+
+const result = await conversationalRetrievalQAChain.invoke({
+  question: "Extract storage to different file",
+  //chat_history: [],
+}, { callbacks: [new MyCallbackHandler()] });
+console.log(result);
